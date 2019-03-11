9f13366fb99a96005f852f921f37fb1273e501c7
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-11 16:08:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,14 @@
 def consolidate_cart(cart)
-  # code here
+   new_hash = {}
+  cart.each do |i|
+  i.each do |name, hash|
+    hash.each do |things|
+      new_hash[name] = things
+      new_hash[name][:count] = cart.count(food)
+    end
+  end
+end
+new_array.uniq
 end
 
 def apply_coupons(cart, coupons)
